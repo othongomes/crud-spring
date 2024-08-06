035f1b3cd6cb77fa4d0e0bab2b6171ef74098f4e
Othon <othonmarcello@hotmail.com>
2024-08-06 00:54:56 -0300

    Formulário: API Spring: Criar Curso (HTTP POST)

--- src/main/java/br/com/crud_spring/controller/CourseController.java
+++ src/main/java/br/com/crud_spring/controller/CourseController.java
@@ -3,8 +3,15 @@ package br.com.crud_spring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.crud_spring.Model.Course;
@@ -24,10 +31,33 @@ public class CourseController {
     // }
 
 
-
+    //@RequestMapping(method = RequestMethod.GET) substituido
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    /* 
+
+    //@RequestMapping(method = RequestMethod.POST) substituido
+    @PostMapping
+    public ResponseEntity<Course> create(@RequestBody Course course) {
+        //System.out.println(course.getName());
+        //return courseRepository.save(course);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(courseRepository.save(course));
+    }
+            
+    */
+
+    //@RequestMapping(method = RequestMethod.POST) substituido
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        //System.out.println(course.getName());
+        return courseRepository.save(course);
+        // return ResponseEntity.status(HttpStatus.CREATED)
+        //     .body(courseRepository.save(course));
     }
     
 }
